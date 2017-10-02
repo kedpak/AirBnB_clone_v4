f55be17de76b53d61792a8d0aaeadac1cb3864b1
kedpak <kedpak@yahoo.com>
2017-10-02 08:54:05 +0000

    fix amenity name listing

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
   let amenList = [];
-  let nameList = [];
   $('INPUT[type=checkbox]').click(function () {
     if ($(this).is(':checked')) {
       amenList.push($(this).attr('data-id'));
-      nameList.push($(this).attr('data-name'));
     } else {
       for (let i = 0; i < amenList.length; i++) {
         if ($(this).attr('data-id') === amenList[i]) {
           delete amenList[i];
-          delete nameList[i];
         }
       }
     }
-    $('.amenities h4').text(nameList);
+    $('.amenities h4').text($(this).attr('data-name'));
   });
+  
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data['status'] === 'OK') {
       $('DIV#api_status').addClass('available');
@@ -22,4 +20,14 @@ $(document).ready(function () {
       $('DIV#api_status').removeClass('available');
     }
   });
+
+  $.ajax({
+     url: 'http://0.0.0.0:5001/api/v1/places_search/',
+     type: 'POST',
+     contentType: 'application/json',
+     dataType: 'json',
+     data: JSON.stringify({}),
+     success; function (data) {
+	 for (let i = 0; i < data.length; i++)
+    
 });
