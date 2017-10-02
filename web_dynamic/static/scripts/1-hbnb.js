dd6b8499e9f6dd0562daf3b708fde87e1020ee05
kedpak <kedpak@yahoo.com>
2017-10-02 06:08:21 +0000

    add div condition to change status of api and fix semistandard styling

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
-   amenList = []
-   $('INPUT[type=checkbox]').click(function () {
-       if ($(this).is(':checked')) {
-	   amenList.push($(this).attr('data-id'));
-       }
-       else {
-	   for (let i = 0; i < amenList.length; i++) {
-	       if ($(this).attr('data-id') === amenList[i]){
-		   delete amenList[i];
-		   }
-	       }
-       }
-       console.log(amenList);
-       $('.amenities h4').text($(this).attr('data-name'));
-    });
-
-
+  let amenList = [];
+  $('INPUT[type=checkbox]').click(function () {
+    if ($(this).is(':checked')) {
+      amenList.push($(this).attr('data-id'));
+    } else {
+      for (let i = 0; i < amenList.length; i++) {
+        if ($(this).attr('data-id') === amenList[i]) {
+          delete amenList[i];
+        }
+      }
+    }
+    $('.amenities h4').text($(this).attr('data-name'));
+  });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
-   amenList = []
-   $('INPUT[type=checkbox]').click(function () {
-       if ($(this).is(':checked')) {
-	      amenList.push($(this).attr('data-id'));
-       }
-       else {
-	      for (let i = 0; i < amenList.length; i++) {
-		         if ($(this).attr('data-id') === amenList[i]){
-			        delete amenList[i];
-			        }
-		         }
-       }
-       console.log(amenList);
-       $('.amenities h4').text($(this).attr('data-name'));
-   });
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-	console.log("TESST")
-    });
-
+  let amenList = [];
+  $('INPUT[type=checkbox]').click(function () {
+    if ($(this).is(':checked')) {
+      amenList.push($(this).attr('data-id'));
+    } else {
+      for (let i = 0; i < amenList.length; i++) {
+        if ($(this).attr('data-id') === amenList[i]) {
+          delete amenList[i];
+        }
+      }
+    }
+    $('.amenities h4').text($(this).attr('data-name'));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data['status'] === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
 });
